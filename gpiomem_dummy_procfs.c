5e62188c63337008bb9ba6d649dac9c3d547a3e0
Thomas Fjellstrom <thomas@fjellstrom.ca>
2018-04-02 09:14:10 -0600

    add extra debug log to proc llseek

--- gpiomem_dummy_procfs.c
+++ gpiomem_dummy_procfs.c
@@ -176,8 +176,9 @@ loff_t proc_llseek(struct file *filp, loff_t offset, int whence)
          break;
    }
 
+   printk(KERN_INFO LOG_PREFIX "llseek: orig off=%lld whence=%s\n", offset, whence_str[whence]);
    new_pos = my_max(0, my_min(new_pos, RANGES_SIZE));
-   printk(KERN_INFO LOG_PREFIX "llseek: off=%lld whence=%s\n", offset, whence_str[whence]);
+   printk(KERN_INFO LOG_PREFIX "llseek: real off=%lld whence=%s\n", new_pos, whence_str[whence]);
 
    filp->f_pos = new_pos;
 
