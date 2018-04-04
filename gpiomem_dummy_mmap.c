1464884443ec06c895ce1efc49eab49d0f194ffd
Thomas Fjellstrom <thomas@fjellstrom.ca>
2018-04-04 14:39:22 -0600

    try removing a couple flags.

--- gpiomem_dummy_mmap.c
+++ gpiomem_dummy_mmap.c
@@ -14,7 +14,7 @@ static void mmap_open(struct vm_area_struct* vma)
    printk(KERN_DEBUG LOG_PREFIX "mmap_open\n");
 
    //vma->vm_flags = (vma->vm_flags | VM_DONTEXPAND | VM_DONTCOPY | VM_DONTDUMP | VM_IO | VM_MAYREAD | VM_MIXEDMAP);
-   vma->vm_flags |= VM_READ | VM_MAYREAD | VM_IO | VM_DONTCOPY | VM_DONTDUMP | VM_DONTEXPAND | VM_MIXEDMAP;
+   vma->vm_flags |= VM_READ | VM_MAYREAD | VM_DONTCOPY | VM_DONTDUMP | VM_DONTEXPAND /*| VM_IO | VM_MIXEDMAP*/;
    vma->vm_flags &= ~(VM_WRITE | VM_MAYWRITE);
 }
 
