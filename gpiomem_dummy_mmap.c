4469809ba5ef20333c0651eee336cec9858be3a0
Thomas Fjellstrom <thomas@fjellstrom.ca>
2018-04-04 16:58:05 -0600

    re-remove write flag on map

--- gpiomem_dummy_mmap.c
+++ gpiomem_dummy_mmap.c
@@ -14,8 +14,8 @@ static void mmap_open(struct vm_area_struct* vma)
    printk(KERN_DEBUG LOG_PREFIX "mmap_open\n");
 
    //vma->vm_flags = (vma->vm_flags | VM_DONTEXPAND | VM_DONTCOPY | VM_DONTDUMP | VM_IO | VM_MAYREAD | VM_MIXEDMAP);
-   vma->vm_flags |= VM_READ | VM_DONTCOPY | VM_DONTDUMP | VM_DONTEXPAND | VM_IO | VM_MIXEDMAP | VM_WRITE;
-   //vma->vm_flags &= ~(VM_MAYWRITE | VM_WRITE);
+   vma->vm_flags |= VM_READ | VM_DONTCOPY | VM_DONTDUMP | VM_DONTEXPAND | VM_IO | VM_MIXEDMAP /*| VM_WRITE*/;
+   vma->vm_flags &= ~(/*VM_MAYWRITE | */VM_WRITE);
    vma->vm_page_prot = pgprot_noncached(vm_get_page_prot(vma->vm_flags));
 }
 
