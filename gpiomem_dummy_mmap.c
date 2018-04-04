09c964d6f36c45c982d9021548ba185206a3f192
Thomas Fjellstrom <thomas@fjellstrom.ca>
2018-04-04 14:24:28 -0600

    try removing write access in mkwrite...

--- gpiomem_dummy_mmap.c
+++ gpiomem_dummy_mmap.c
@@ -119,6 +119,7 @@ int mmap_mkwrite(struct vm_fault *vmf)
    printk(KERN_DEBUG LOG_PREFIX "page_mkwrite!\n");
    lock_page(vmf->page);
    set_page_dirty(vmf->page);
+   vmf->vma->vm_flags &= ~VM_WRITE;
    return VM_FAULT_LOCKED;
 }
 
