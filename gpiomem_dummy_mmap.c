069baadc2d10784e604c011135d2bb7d2ff46c1e
Thomas Fjellstrom <thomas@fjellstrom.ca>
2018-04-04 21:12:53 -0600

    locked?

--- gpiomem_dummy_mmap.c
+++ gpiomem_dummy_mmap.c
@@ -14,7 +14,7 @@ static void mmap_open(struct vm_area_struct* vma)
    printk(KERN_DEBUG LOG_PREFIX "mmap_open\n");
 
    //vma->vm_flags = (vma->vm_flags | VM_DONTEXPAND | VM_DONTCOPY | VM_DONTDUMP | VM_IO | VM_MAYREAD | VM_MIXEDMAP);
-   vma->vm_flags |= VM_DONTCOPY | VM_DONTDUMP | VM_DONTEXPAND | VM_MAYWRITE | VM_SHARED;// | VM_WRITE | VM_IO;
+   vma->vm_flags |= VM_DONTCOPY | VM_DONTDUMP | VM_DONTEXPAND | VM_MAYWRITE | VM_SHARED | VM_LOCKED;// | VM_WRITE | VM_IO;
    //vma->vm_flags &= ~(/*VM_MAYWRITE | */VM_WRITE);
    vma->vm_page_prot = pgprot_noncached(vm_get_page_prot(vma->vm_flags));
 }
